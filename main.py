bc9097df0348b4e19c8b2f5a2ac29713b256f94f
Pat Broders <patrick,broders73@ed.cna.nl.ca>
2023-03-21 20:28:03 -0230

    First real addition to the project. Added I/O functions, txt file, csv file and tested for input validation.

--- main.py
+++ main.py
@@ -1,6 +1,74 @@
+import sys
+import csv
+
+
+def load_bank():
+    while True:
+        try:
+            with open("bank.txt") as file:
+                bank = float(file.readline())
+            print(f"\nMoney: {bank}")
+            wager = float(input("Bet amount: "))
+            if wager <= 5:
+                print("Bet amount must be greater than $5.")
+                continue
+            if wager > bank:
+                print("Bet amount must be lower than your total amount of money")
+                continue
+            else:
+                bank -= wager
+                save_bank(bank)
+                break
+        except ValueError:
+            print("Please enter a valid number greater than 5.")
+        except FileNotFoundError:
+            print("File not found. Closing program.")
+            sys.exit(1)
+        except Exception as e:
+            print("Unknown error occurred. Closing program.")
+            print(type(e), e)
+            sys.exit(1)
+    return bank
+
+
+def save_bank(bank):
+    try:
+        with open("bank.txt", "w") as file:
+            file.write(f"{bank}")
+    except Exception as e:
+        print("Unknown error occurred. Closing program.")
+        print(type(e), e)
+        sys.exit(1)
+
+
+def load_deck():
+    try:
+        deck = []
+        with open("deck.csv", newline="") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                card = [row[0], row[1], int(row[2])]
+                deck.append(card)
+        return deck
+    except FileNotFoundError:
+        print("File not found. Closing program.")
+        sys.exit(1)
+    except Exception as e:
+        print("Unknown error occurred. Closing program.")
+        print(type(e), e)
+        sys.exit(1)
+
+
 def main():
 
-    print('Hi')
+    print("BLACKJACK!")
+    print("Blackjack payout is 3:2")
+
+    bank = load_bank()
+    deck = load_deck()
+
+    print(f"{bank}")
+    print(f"{deck}")
 
 
 if __name__ == '__main__':
